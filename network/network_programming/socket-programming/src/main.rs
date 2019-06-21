47d348b146bf7d3da3841dbf74a5c94756a67a65
mkusaka <hinoshita1992@gmail.com>
2019-06-22 00:55:31 +0900

    add tcp server and use it
    ex) `cargo run tcp 127.0.0.1 33333` & `telnet 127.0.0.1 33333`

--- network/network_programming/socket-programming/src/main.rs
+++ network/network_programming/socket-programming/src/main.rs
@@ -2,15 +2,15 @@ use std::env;
 #[macro_use]
 extern crate log;
 
-mod tcp_client;
+// mod tcp_client;
 mod tcp_server;
-mod udp_client;
-mod udp_server;
+// mod udp_client;
+// mod udp_server;
 
 fn main() {
     env::set_var("RUST_LOG", "debug");
     env_logger::init();
-    let args: Vec<String> = env::args().collect()
+    let args: Vec<String> = env::args().collect();
 
     if args.len() != 4 {
         error!("Please specify [tcp|udp] [server|client] [addr:port].");
@@ -25,6 +25,7 @@ fn main() {
         "tcp" => match role {
             "server" => {
                 // TODO: call tcp server
+                tcp_server::serve(address).unwrap_or_else(|e| error!("{}", e));
             }
             "client" => {
                 // TODO: call tcp client
