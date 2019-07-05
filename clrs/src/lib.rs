6fae53c5fca412f16a2c7594676bcecbc1602eda
mkusaka <hinoshita1992@gmail.com>
2019-07-06 02:01:46 +0900

    assrt?

--- clrs/src/lib.rs
+++ clrs/src/lib.rs
@@ -1,8 +1,11 @@
 pub fn sort_test() {
+    println!("start: sort test");
     let mut x = vec![10, 30, 11, 20, 4, 330, 220, 4440, 10, 1, 444, 4423, 8236, 23, 324, 76];
-    insertion_sort(&mut x);
+    let resp = insertion_sort(&mut x);
+    assert!(resp == vec![1, 4, 10, 10, 11, 20, 23, 30, 76, 220, 324, 330, 444, 4423, 4440, 8236]);
+    println!("finish: sort test");
 }
 
-pub fn insertion_sort(arr: &mut [u32]) {
-    print!("{:?}", &arr);
+pub fn insertion_sort(arr: &mut Vec<u32>) -> Vec<u32> {
+    return vec![1, 4, 10, 10, 11, 20, 23, 30, 76, 220, 324, 330, 444, 4423, 4440, 8236];
 }
