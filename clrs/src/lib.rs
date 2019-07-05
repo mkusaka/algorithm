23775318828a1565591d04b89fe7918790b37068
mkusaka <hinoshita1992@gmail.com>
2019-07-06 02:28:08 +0900

    first insersion sort

--- clrs/src/lib.rs
+++ clrs/src/lib.rs
@@ -1,11 +1,22 @@
 pub fn sort_test() {
     println!("start: sort test");
-    let mut x = vec![10, 30, 11, 20, 4, 330, 220, 4440, 10, 1, 444, 4423, 8236, 23, 324, 76];
-    let resp = insertion_sort(&mut x);
-    assert!(resp == vec![1, 4, 10, 10, 11, 20, 23, 30, 76, 220, 324, 330, 444, 4423, 4440, 8236]);
+    let x = vec![10, 30, 11, 20, 4, 330, 220, 4440, 10, 1, 444, 4423, 8236, 23, 324, 76];
+    let resp = insertion_sort(x);
+    assert_eq!(resp, vec![1, 4, 10, 10, 11, 20, 23, 30, 76, 220, 324, 330, 444, 4423, 4440, 8236]);
     println!("finish: sort test");
 }
 
-pub fn insertion_sort(arr: &mut Vec<u32>) -> Vec<u32> {
-    return vec![1, 4, 10, 10, 11, 20, 23, 30, 76, 220, 324, 330, 444, 4423, 4440, 8236];
+pub fn insertion_sort(arr: Vec<u32>) -> Vec<u32> {
+    let mut output = arr.clone();
+    for (_, j) in (2..output.len()).enumerate() {
+        let key = output[j];
+        let mut i = j - 1;
+        while i > 0 && output[i] > key {
+            println!("{}", output[i]);
+            output[i + 1] = output[i];
+            i = i - 1;
+        }
+        output[i + 1] = key;
+    }
+    return output
 }
