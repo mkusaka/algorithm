348fbdd1565c93fda28ace4d1c68de2747c709c6
mkusaka <hinoshita1992@gmail.com>
2019-07-07 12:11:50 +0900

    change directory and add script

--- clrs/src/lib.rs
+++ clrs/src/lib.rs
@@ -2,21 +2,23 @@ pub fn sort_test() {
     println!("start: sort test");
     let x = vec![10, 30, 11, 20, 4, 330, 220, 4440, 10, 1, 444, 4423, 8236, 23, 324, 76];
     let resp = insertion_sort(x);
+    println!("{:?}", resp);
+    // fail test
     assert_eq!(resp, vec![1, 4, 10, 10, 11, 20, 23, 30, 76, 220, 324, 330, 444, 4423, 4440, 8236]);
     println!("finish: sort test");
 }
 
 pub fn insertion_sort(arr: Vec<u32>) -> Vec<u32> {
     let mut output = arr.clone();
-    for (_, j) in (2..output.len()).enumerate() {
+    for j in 2..output.len() {
         let key = output[j];
         let mut i = j - 1;
         while i > 0 && output[i] > key {
-            println!("{}", output[i]);
             output[i + 1] = output[i];
             i = i - 1;
         }
         output[i + 1] = key;
     }
+    println!("{:?}", output);
     return output
 }
