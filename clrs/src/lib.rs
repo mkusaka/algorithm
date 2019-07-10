3bb361c85c77f4e287305355e10552e025a1deac
mkusaka <hinoshita1992@gmail.com>
2019-07-11 00:54:57 +0900

    add test for insersion sort

--- clrs/src/lib.rs
+++ clrs/src/lib.rs
@@ -20,3 +20,15 @@ pub fn insertion_sort(arr: Vec<u32>) -> Vec<u32> {
     }
     return output;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::insertion_sort;
+
+    #[test]
+    fn is_valid_insertion_sort() {
+        let x = vec![10, 30, 11, 20, 4, 330, 220, 4440, 10, 1, 444, 4423, 8236, 23, 324, 76];
+        let y = insertion_sort(x);
+        assert_eq!(y, vec![1, 4, 10, 10, 11, 20, 23, 30, 76, 220, 324, 330, 444, 4423, 4440, 8236]);
+    }
+}
