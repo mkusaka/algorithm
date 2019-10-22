bedd51c70e73d2e46e04a0a49def5161a3ada9da
mkusaka <hinoshita1992@gmail.com>
2019-10-22 21:46:24 +0900

    add some

---
+++ languages/rust/kagura_note/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+/**
+ * @type {import("webpack").Configuration}
+ */
+module.exports = {
+  resolve: {
+    extensions: [".js"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "./src/index.html")
+    }),
+    // @ts-ignore
+    new WasmPackPlugin({
+      crateDirectory: path.join(__dirname, "./")
+    })
+  ]
+}
