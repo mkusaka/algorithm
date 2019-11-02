953d3ac410d125b1d9868f8aaad59bcd4c927ae5
mkusaka <hinoshita1992@gmail.com>
2019-11-03 00:51:06 +0900

    range close

--- languages/golang/astaxie_gobook/src/main.go
+++ languages/golang/astaxie_gobook/src/main.go
@@ -1,22 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func sum(a []int, c chan int) {
-	total := 0
-	for _, v := range a {
-		total += v
+func fibonacci(n int, c chan int) {
+	x, y := 1, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
 	}
-	c <- total // 合計をcチャネルに投げる
+	close(c)
 }
 
 func main() {
-	a := []int{7, 2, 8, -9, 4, 0}
-
-	c := make(chan int)
-	go sum(a[:len(a)/2], c)
-	go sum(a[len(a)/2:], c)
-	x, y := <-c, <-c // cから合計を受け取る
-
-	fmt.Println(x, y, x+y)
+	c := make(chan int, 10)
+	go fibonacci(cap(c), c)
+	for i := range c {
+		fmt.Println(i)
+	}
 }
