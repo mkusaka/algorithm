175d9e7e40057743941a4dd9345e3649c4849474
mkusaka <hinoshita1992@gmail.com>
2019-11-03 00:44:12 +0900

    basic channel

--- languages/golang/astaxie_gobook/src/main.go
+++ languages/golang/astaxie_gobook/src/main.go
@@ -1,14 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
+
+func sum(a []int, c chan int) {
+	total := 0
+	for _, v := range a {
+		total += v
+	}
+	c <- total // 合計をcチャネルに投げる
+}
 
 func main() {
-	var x float64 = 3.4
-	v := reflect.ValueOf(x)
-	fmt.Println("type:", v.Type())
-	fmt.Println("kind is float64:", v.Kind() == reflect.Float64)
-	fmt.Println("value:", v.Float())
+	a := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(a[:len(a)/2], c)
+	go sum(a[len(a)/2:], c)
+	x, y := <-c, <-c // cから合計を受け取る
+
+	fmt.Println(x, y, x+y)
 }
