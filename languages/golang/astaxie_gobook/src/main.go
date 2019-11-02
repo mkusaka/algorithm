6013f51ba25a36f85ca0ea0004ff82e439dd80ca
mkusaka <hinoshita1992@gmail.com>
2019-11-02 23:56:37 +0900

    method

--- languages/golang/astaxie_gobook/src/main.go
+++ languages/golang/astaxie_gobook/src/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+type Rectangle struct {
+	width, height float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (r Rectangle) area() float64 {
+	return r.width * r.height
+}
+
+func (c Circle) area() float64 {
+	return c.radius * c.radius * math.Pi
+}
 
 func main() {
-	fmt.Println("hoge")
+	r1 := Rectangle{12, 2}
+	r2 := Rectangle{9, 4}
+	c1 := Circle{10}
+	c2 := Circle{25}
+
+	fmt.Println(r1.area())
+	fmt.Println(r2.area())
+	fmt.Println(c1.area())
+	fmt.Println(c2.area())
 }
