f0add855faf0a32b86358ead88dd49f390f665d1
mkusaka <hinoshita1992@gmail.com>
2019-11-04 18:40:28 +0900

    mongo

--- languages/golang/astaxie_gobook/src/main.go
+++ languages/golang/astaxie_gobook/src/main.go
@@ -1,96 +1,40 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"log"
 
-	_ "github.com/go-sql-driver/mysql"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
-func main() {
-	db, err := sql.Open("mysql", "root:mysql@/mtest?charset=utf8")
-	checkErr(err)
-
-	//データの挿入
-	stmt, err := db.Prepare("INSERT userinfo SET username=?,departname=?,created=?")
-	checkErr(err)
-
-	res, err := stmt.Exec("author", "kaihatsu", "2012-12-09")
-	checkErr(err)
-
-	id, err := res.LastInsertId()
-	checkErr(err)
-
-	fmt.Println(id)
-	//データの更新
-	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
-	checkErr(err)
-
-	res, err = stmt.Exec("author", id)
-	checkErr(err)
-
-	affect, err := res.RowsAffected()
-	checkErr(err)
-
-	fmt.Println(affect)
-
-	//データの検索
-	rows, err := db.Query("SELECT * FROM userinfo")
-	checkErr(err)
-
-	for rows.Next() {
-		var uid int
-		var username string
-		var department string
-		var created string
-		err = rows.Scan(&uid, &username, &department, &created)
-		checkErr(err)
-		fmt.Println(uid)
-		fmt.Println(username)
-		fmt.Println(department)
-		fmt.Println(created)
-	}
-
-	//データの削除
-	stmt, err = db.Prepare("delete from userinfo where uid=?")
-	checkErr(err)
-
-	res, err = stmt.Exec(id)
-	checkErr(err)
-
-	affect, err = res.RowsAffected()
-	checkErr(err)
-
-	fmt.Println(affect)
-
-	db.Close()
-
+type Person struct {
+	Name  string
+	Phone string
 }
 
-func checkErr(err error) {
+func main() {
+	session, err := mgo.Dial("localhost")
 	if err != nil {
 		panic(err)
 	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("test").C("people")
+	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
+		&Person{"Cla", "+55 53 8402 8510"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Phone:", result.Phone)
 }
-
-/*
-CREATE DATABASE mtest;
-
-use mtest;
-
-CREATE TABLE `userinfo` (
-    `uid` INT(10) NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(64) NULL DEFAULT NULL,
-    `departname` VARCHAR(64) NULL DEFAULT NULL,
-    `created` DATE NULL DEFAULT NULL,
-    PRIMARY KEY (`uid`)
-);
-
-CREATE TABLE `userdetail` (
-    `uid` INT(10) NOT NULL DEFAULT '0',
-    `intro` TEXT NULL,
-    `profile` TEXT NULL,
-    PRIMARY KEY (`uid`)
-);
-
-*/
